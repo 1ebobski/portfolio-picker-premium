57933b4edee388e498daf60e1684e8a6ff6b091b
Fedor Lukhmanov <fedor.lukhmanov@ya.ru>
2021-09-22 15:34:32 +0300

    first release

---
+++ src/js/constants/matrixes/matrix_rub.js
@@ -0,0 +1,82 @@
+export const MATRIX_RUB = {
+  "a.1": {
+    shares: [50, 40],
+    portfolios: [
+      ["БПИФ", "БПИФ"],
+      ["БПИФ", "БПИФ"],
+    ],
+  },
+  "a.2": {
+    shares: [50, 50],
+    portfolios: [
+      ["Finex 1", "Finex 2"],
+      ["Finex 1", "Finex 2"],
+    ],
+  },
+  "a.3": {
+    shares: [100, 100],
+    portfolios: [
+      ["Самит N", "Самит N"],
+      ["Самит N", "Самит N"],
+    ],
+  },
+  "b.1": {
+    shares: [25, 25],
+    portfolios: [
+      ["БПИФ", "БПИФ"],
+      ["Finex 1", "Finex 2"],
+    ],
+  },
+  "b.2": {
+    shares: [50, 50],
+    portfolios: [
+      ["Самит N", "СПБ N"],
+      ["Самит N", "СПБ N"],
+    ],
+  },
+  "b.3": { shares: [50, 50], portfolios: [["ПИФ 1", "ПИФ 2"]] },
+  "b.4": { shares: [100, 100], portfolios: [["КОРП", "КОРП"]] },
+  "c.1": {
+    shares: [30, 30],
+    portfolios: [
+      ["БПИФ", "БПИФ"],
+      ["Finex 1", "Finex 2"],
+    ],
+  },
+  "c.2": {
+    shares: [35, 20],
+    portfolios: [
+      ["Самит N", "СПБ N"],
+      ["Самит N", "СПБ N"],
+    ],
+  },
+  "c.3": { shares: [40, 30], portfolios: [["ПИФ 1", "ПИФ 2"]] },
+  "c.4": { shares: [50, 50], portfolios: [["ДУ СП", "ДУ СП"]] },
+  "c.5": { shares: [100, 100], portfolios: [["КОРП", "КОРП"]] },
+  "d.1": {
+    shares: [15, 10],
+    portfolios: [
+      ["БПИФ", "БПИФ"],
+      ["Finex 1", "Finex 2"],
+    ],
+  },
+  "d.2": { shares: [20, 20], portfolios: [["Самит N", "Самит N"]] },
+  "d.3": { shares: [20, 20], portfolios: [["СПБ N", "СПБ N"]] },
+  "d.4": { shares: [30, 15], portfolios: [["КОРП", "КОРП"]] },
+  "d.5": { shares: [30, 30], portfolios: [["GLD", "GLD"]] },
+  "d.6": { shares: [50, 30], portfolios: [["ЗПИФ", "ЗПИФ"]] },
+  "d.7": { shares: [100, 100], portfolios: [["СП", "СП"]] },
+  "e.1": {
+    shares: [70, 30],
+    portfolios: [
+      ["EO", "EO"],
+      ["EO", "EO"],
+    ],
+  },
+  "e.2": { shares: [10, 5], portfolios: [["КОРП", "КОРП"]] },
+  "e.3": { shares: [20, 10], portfolios: [["СПБ N", "СПБ N"]] },
+  "e.4": { shares: [20, 30], portfolios: [["Самит N", "Самит N"]] },
+  "e.5": { shares: [30, 20], portfolios: [["GLD", "GLD"]] },
+  "e.6": { shares: [40, 40], portfolios: [["ЗПИФ", "ЗПИФ"]] },
+  "e.7": { shares: [100, 100], portfolios: [["СП", "СП"]] },
+};
---
+++ src/js/constants/scales.js
@@ -0,0 +1,21 @@
+// columns are "protect savings", "try and learn", "investing", "trading"
+// rows are "return +8%, drop -5%, return +13%, drop -10%", "return +20%, drop -15%", "return max, drop any"
+
+const RISK_MATRIX = [
+  [1, 3, 3, 3],
+  [2, 3, 4, 5],
+  [2, 4, 5, 6],
+  [2, 5, 6, 7],
+];
+
+// keys are keys and values are money breakpoints in rubles
+
+const MONEY_SCALE = {
+  a: 100000,
+  b: 700000,
+  c: 3000000,
+  d: 10000000,
+  e: 100000000,
+};
+
+export { RISK_MATRIX, MONEY_SCALE };
